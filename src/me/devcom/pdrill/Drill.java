4d558bfe5aa407fb581f11cb4933605d32d44d05
Kamil Pietka <ardmax1@o2.pl>
2011-08-01 00:44:32 +0200

    v0.3.1

--- src/me/devcom/pdrill/Drill.java
+++ src/me/devcom/pdrill/Drill.java
@@ -1,5 +1,6 @@
 package me.devcom.pdrill;
 
+
 import java.util.logging.Logger;
 
 import org.bukkit.Location;
@@ -12,10 +13,9 @@ import org.bukkit.util.Vector;
 
 public class Drill {
 	PDrill plugin;
-
-	//LinkManager linkManager = null;
+	
 	boolean linked = false;
-
+	
 	public Player owner;
 	public Location position;
 	public Block block;
@@ -23,7 +23,8 @@ public class Drill {
 	public boolean enabled;
 	public JobManager JobMG;
 	public FuelManager FuelMG;
-
+	public BlockPlaceManager blockPlaceManager = null;
+	
 	public Integer blockCount = 0;
 	public Location forwardDir;
 	public Location backwardDir;
@@ -34,22 +35,25 @@ public class Drill {
 
 	public final Logger logger = Logger.getLogger("Minecraft");
 
+	public boolean isVirtual = false;
+	public Fuel virtualFuel = null;
+	
 	public Drill(PDrill instance, Player player, Block block, Integer id){
 		plugin = instance;
 		this.owner = player;
 		this.block = block;
 		this.id = id;
-
+		
 		this.enable();
-
+		
 		JobMG = new JobManager( this );
 		FuelMG = new FuelManager( this );
-
+		
 		World world = block.getWorld();
 
 		upDir = new Location(world, 0,1,0);
 		downDir = new Location( world, 0,-1,0);
-
+		
 		Vector direction = player.getLocation().getDirection();
 		if (direction.getZ() > .5d) {
 			forwardDir = new Location(world, 0, 0, 1);
@@ -73,45 +77,52 @@ public class Drill {
 
 		leftDir = new Location(world, rightDir.getBlockX(), rightDir.getBlockY(), rightDir.getBlockZ());
 		leftDir.multiply(-1);
-	}
 
+	}
+	
 	public void update(){
 		JobMG.doJob();
 	}
 
-	public boolean moveToLocation(Location nextLoc) {
+	public boolean moveInDirection(String direction) {
 		if( FuelMG.getFuelLevel() > 0){
 			Fuel fuel = FuelMG.fuel();
-
+			
+			
+			Location nextLoc = block.getLocation().add( getDirection(direction) );
 			Block nextBlock = block.getWorld().getBlockAt( nextLoc );
 			dropBlock( nextBlock );
 			nextBlock.setTypeId( Material.FURNACE.getId() );
-
+			
 			if( blockCount >= fuel.fuelConsumptionBlockCount){
-				FuelMG.consumeFuel();
+				FuelMG.consumeFuel( fuel.fuelConsumptionFuelCount );
 				blockCount = 0;
 			}
 			FuelMG.swapFuel( nextBlock );
-
+			
 			block.setTypeId( 0 );
 			block = nextBlock;
-
+			
+			if( blockPlaceManager != null ){
+				blockPlaceManager.tick();
+			}
+			
 			blockCount++;
 			return true;
 		}
 		return false;
 	}
 
-	private void dropBlock(Block nextBlock) {
+	public void dropBlock(Block nextBlock) {
 		if( plugin.configManager.dropItemNaturally){
 			if( nextBlock.getTypeId() != Material.AIR.getId() ){
 				boolean drop = false;
 				Integer dropId = nextBlock.getTypeId();
-
+				
 				if(plugin.configManager.drops.containsKey( dropId )){
 					dropId = plugin.configManager.drops.get( dropId );
 				}
-
+				
 				if( plugin.configManager.dropItemList.isEmpty() ){
 					drop = true;
 				}else if( plugin.configManager.dropItemList.contains( dropId )){
@@ -127,7 +138,7 @@ public class Drill {
 
 	public Location getDirection(String direction) {
 		Location dirLoc = null;
-
+		
 		if( direction.equals("f") ){
 			dirLoc = forwardDir;
 		}else if( direction.equals("b") ){
@@ -141,7 +152,7 @@ public class Drill {
 		}else if( direction.equals("d") ){
 			dirLoc = downDir;
 		}
-
+		
 		return dirLoc;
 	}
 
@@ -149,9 +160,30 @@ public class Drill {
 		owner.sendMessage( "Drill deactivated! [" + id +"]" );
 		enabled = false;
 	}
-
+	
 	public void enable() {
 		owner.sendMessage( "Drill activated! [" + id +"]" );
 		enabled = true;
+	}
+
+	public Block getNextBlock(String direction) {
+		Block drillBlock = block;
+		Location location = drillBlock.getLocation();
+		World world = drillBlock.getWorld();
+		
+		int x = location.getBlockX();
+		int y = location.getBlockY();
+		int z = location.getBlockZ();
+		
+		Location nextLoc = new Location(world, x, y, z);
+		nextLoc.add( getDirection( direction ));
+		
+		return world.getBlockAt( nextLoc );
+	}
+
+	public boolean canMoveInDirection(String direction) {
+		Location nextLoc = block.getLocation().add( getDirection(direction) );
+		Block nextBlock = block.getWorld().getBlockAt( nextLoc );
+		return !(plugin.configManager.stopblocks.contains( nextBlock.getTypeId() ) || (plugin.drillManager.getDrillFromBlock( nextBlock ) != null));
 	}
 }
