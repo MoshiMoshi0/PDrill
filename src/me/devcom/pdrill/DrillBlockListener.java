9c166543daf2007a4a454172ddcfc52c451fc0de
Remko Plantenga <sonata82@gmail.com>
2012-03-11 11:10:31 +0100

    Changes for new Config and Event API

--- src/me/devcom/pdrill/DrillBlockListener.java
+++ src/me/devcom/pdrill/DrillBlockListener.java
@@ -5,36 +5,38 @@ import java.util.ArrayList;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockListener;
 
-public class DrillBlockListener extends BlockListener {
-	public static PDrill plugin;
-	
-	public final DrillManager drillManager;
-	public final ArrayList< Drill > DrillDB;
-	
-	public DrillBlockListener( PDrill instance ){
-		plugin = instance;
-		drillManager = plugin.drillManager;
-		DrillDB = drillManager.DrillDB;
-	}
-	
-	public void onBlockBreak(BlockBreakEvent event){
-		Block block = event.getBlock();
-		Player player = event.getPlayer();
-		
-		if(block.getType().equals( Material.FURNACE )){
-			Drill drill = drillManager.getDrillFromBlock(block);
-			
-			if( drill != null ){
-				drillManager.remove( drill );
-				player.sendMessage("Drill removed! [" + drill.id +"]");
-				
-				if( drill.linked ){
-					drill.updateParentOnBreak();
-				}
-			}
-		}
-	}
+public class DrillBlockListener implements Listener {
+    public static PDrill plugin;
+
+    public final DrillManager drillManager;
+    public final ArrayList<Drill> DrillDB;
+
+    public DrillBlockListener(PDrill instance) {
+        plugin = instance;
+        drillManager = plugin.drillManager;
+        DrillDB = drillManager.DrillDB;
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        Block block = event.getBlock();
+        Player player = event.getPlayer();
+
+        if (block.getType().equals(Material.FURNACE)) {
+            Drill drill = drillManager.getDrillFromBlock(block);
+
+            if (drill != null) {
+                drillManager.remove(drill);
+                player.sendMessage("Drill removed! [" + drill.id + "]");
+
+                if (drill.linked) {
+                    drill.updateParentOnBreak();
+                }
+            }
+        }
+    }
 }
--- src/me/devcom/pdrill/PDrill.java
+++ src/me/devcom/pdrill/PDrill.java
@@ -1,64 +1,69 @@
 package me.devcom.pdrill;
 
-
 import java.io.File;
 import java.util.logging.Logger;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 
 public class PDrill extends JavaPlugin {
-	
-	public BukkitScheduler sheduler;
 
-	public final ConfigurationManager configManager = new ConfigurationManager(this, "config.yml", "plugins/PDrill/");
-	public final DrillManager drillManager = new DrillManager( this );
-	private final CommandProcessor commandProcessor = new CommandProcessor(this);
-	
-	public final DrillBlockListener blockListener = new DrillBlockListener( this );
-	public final DrillPlayerListener playerListener = new DrillPlayerListener( this );
-	public final Logger logger = Logger.getLogger("Minecraft");
-	public final String prefix = "[PDrill] ";
+    public BukkitScheduler sheduler;
 
-	@Override 
-	public void onDisable(){
-		PluginDescriptionFile pdfFile = this.getDescription();
-		this.logger.info( pdfFile.getName() + " [ver: " + pdfFile.getVersion() + "] is disabled!");
-	}
-	
-	@Override 
-	public void onEnable(){
-		final PDrill plugin = this;
-		sheduler = this.getServer().getScheduler();
-		PluginManager pm = this.getServer().getPluginManager();
-		pm.registerEvent( Event.Type.PLAYER_INTERACT, playerListener, Event.Priority.Normal, this);
-		pm.registerEvent( Event.Type.BLOCK_BREAK, blockListener, Event.Priority.Normal, this);
-		
-		File dir = new File( "plugins/PDrill" );
-		if(!dir.exists()){
-			dir.mkdirs();
-		}	
-		
-		configManager.load();
-		
-		Runnable runnable = new Runnable(){
-			public void run(){
-				plugin.drillManager.updateDrills();
-			}
-		};
-		sheduler.scheduleSyncRepeatingTask( plugin, runnable, (long)(configManager.blockSpeed * 20), (long)(configManager.blockSpeed * 20));
-		
-		PluginDescriptionFile pdfFile = this.getDescription();
-		logger.info( prefix + pdfFile.getName() + " [ver: " + pdfFile.getVersion() + "] is enabled!");
-	}
-	
-	//@Override
-	public boolean onCommand( CommandSender sender, Command cmd, String commandLabel, String[] args){
-		return commandProcessor.process( sender, commandLabel, args);
-	}
+    public ConfigurationManager configManager;
+    public final DrillManager drillManager = new DrillManager(this);
+    private final CommandProcessor commandProcessor = new CommandProcessor(this);
+
+    public final DrillBlockListener blockListener = new DrillBlockListener(this);
+    public final DrillPlayerListener playerListener = new DrillPlayerListener(
+            this);
+    public final Logger logger = Logger.getLogger("Minecraft");
+    public final String prefix = "[PDrill] ";
+
+    @Override
+    public void onDisable() {
+        PluginDescriptionFile pdfFile = this.getDescription();
+        this.logger.info(pdfFile.getName() + " [ver: " + pdfFile.getVersion()
+                + "] is disabled!");
+    }
+
+    @Override
+    public void onEnable() {
+        final PDrill plugin = this;
+        sheduler = this.getServer().getScheduler();
+        PluginManager pm = this.getServer().getPluginManager();
+        pm.registerEvents(playerListener, this);
+        pm.registerEvents(blockListener, this);
+
+        File dir = new File("plugins/PDrill");
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+
+        configManager = new ConfigurationManager(this, getConfig());
+        configManager.load();
+
+        Runnable runnable = new Runnable() {
+            public void run() {
+                plugin.drillManager.updateDrills();
+            }
+        };
+        sheduler.scheduleSyncRepeatingTask(plugin, runnable,
+                (long) (configManager.blockSpeed * 20),
+                (long) (configManager.blockSpeed * 20));
+
+        PluginDescriptionFile pdfFile = this.getDescription();
+        logger.info(prefix + pdfFile.getName() + " [ver: "
+                + pdfFile.getVersion() + "] is enabled!");
+    }
+
+    // @Override
+    public boolean onCommand(CommandSender sender, Command cmd,
+            String commandLabel, String[] args) {
+        return commandProcessor.process(sender, commandLabel, args);
+    }
 }
